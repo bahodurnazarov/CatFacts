07662086719c3c6600247156c68a8d7f7387aff1
Bahodur J. Nazarov <NazarovB@HUMO.local>
2023-05-18 15:16:04 +0500

    bot connected to service

--- cmd/catFacts/main.go
+++ cmd/catFacts/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	run "github.com/bahodurnazarov/CatFacts/internal/bot"
 	"github.com/bahodurnazarov/CatFacts/internal/handler"
 )
 
 func main() {
+	go run.Route()
 	handler.Listen()
 }
