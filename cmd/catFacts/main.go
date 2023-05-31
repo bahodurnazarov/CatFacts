3f493cf2249a8e989edac4ae374bf037f0ecdfd7
israilzadeh <firdavs080902@gmail.com>
2023-05-31 17:43:47 +0500

    new

--- cmd/catFacts/main.go
+++ cmd/catFacts/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/bahodurnazarov/CatFacts/internal/Job"
 	"github.com/bahodurnazarov/CatFacts/internal/handler"
 )
 
 func main() {
+	Ping()
 	go Job.Route()
 	handler.Listen()
+}
+
+func Ping() {
+	fmt.Println("ping-pong")
 }
